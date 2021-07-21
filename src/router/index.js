20c8f86e6aab545d1d57b2fbd35beac556a2a709
mineteeraa <patteera_mine@hotmail.com>
2021-07-21 16:18:14 +0700

    Create EventDetails and change the router-link in App

--- src/router/index.js
+++ src/router/index.js
@@ -1,6 +1,8 @@
 import { createRouter, createWebHistory } from 'vue-router'
 import EventList from '../views/EventList.vue'
 import About from '../views/About.vue'
+import EventDetails from '../views/EventDetails.vue'
+
 const routes = [
   {
     path: '/',
@@ -11,6 +13,12 @@ const routes = [
     path: '/about',
     name: 'About',
     component: About
+  },
+  {
+    path: '/event/:id',
+    name: 'EventDetails',
+    component: EventDetails,
+    props: true
   }
 ]
 
